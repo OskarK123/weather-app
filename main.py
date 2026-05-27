a5ebc8ab511b0237cb1e1f46151be2f043d22c4b
OskarK123 <oskar.kowalewski@icloud.com>
2026-05-27 19:42:21 +0200

    pandas implementation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from services.openwather_api import get_weather
 import time
+from services.excel_files import save_to_excel
 
 while True:
     weather = get_weather()
-    print(weather)
+    save_to_excel([weather])
+    print("Udało się pobrać dane")
     time.sleep(10)
 
 
